227d2310f497faebf3f428d5b54c3a8eb3d289e2
Yulia <peresheina.yulia@yandex.ru>
2022-12-26 23:10:19 +0300

    Solution add

--- SolutionCS/Program.cs
+++ SolutionCS/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// написать программу,  которая из имеющегося массива строк формирует массив из строк, длина которых меньше или равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма
+
+
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++) 
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+string[] SortElement(string[] array)
+{
+    int count = 0;
+    string[] newArr = new string[0];
+    for (int i = 0; i < array.Length; i++)
+        if (array[i].Length <= 3)
+        {
+            Array.Resize(ref newArr, count + 1);
+            newArr[count] = array[i];
+            count++;
+        }
+    return newArr;
+}
+string[] FillArray()
+{
+    Console.WriteLine("Введите количество элементов создаваемого строкового массива: ");
+    int size = int.Parse(Console.ReadLine()!);
+    string[] array = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} элемент массива: ");
+        array[i] = Console.ReadLine()!;
+        if (array[i] == string.Empty)
+        {
+            Console.WriteLine("Введите не нулевое значение");
+            i--;
+        }
+    }
+    return array;
+}
+
+string[] incomingArr = FillArray();
+Console.Clear();
+Console.WriteLine("Заданный массив: ");
+PrintArray(incomingArr);
+Console.WriteLine("Получивщийся массив c элементами менее или равно 3 символам: ");
+PrintArray(SortElement(incomingArr));
